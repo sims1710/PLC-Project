54a8ca6714b3855a08227d5e4e72893ac3e5e34d
ShelenGo <shelengo21@gmail.com>
2024-03-10 21:10:22 +0800

    player input and hidden word functions

--- update_hidden_word.c
+++ update_hidden_word.c
@@ -1,6 +1,13 @@
 /* Update the pointer for the hidden word display */
+#include <string.h>
 #include "all_functions.h"
 
 void update_hidden_word(char *hidden_word, char *chosen_word, char input_letter)
 {
+    int i;
+    for(i=0; i<strlen(chosen_word);i++){
+        if(input_letter == chosen_word[i]){
+            hidden_word[i] = input_letter;
+        }
+    }
 }
