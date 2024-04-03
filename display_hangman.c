309a06bd1139ed31b88da28da60e84853c788ef7
Kendrick <kendricknghy@gmail.com>
2024-04-03 14:48:46 +0800

    update

--- display_hangman.c
+++ display_hangman.c
@@ -26,24 +26,60 @@ void display_hangman(char *hidden_word, int *lives, int word_len)
             {
             case 1:
                 /* Score */
+                printf("# Score:");
+                int base = 8;
+                /* TODO: print out score value and  give len of score*/
+                int score_len;
+                int i;
+                for (i = base + score_len; i < window_width - 1; i++)
+                {
+                    printf(" ");
+                }
+                printf("#");
                 break;
             case 2:
                 printf("#                                +---+  #");
                 break;
             case 3:
                 /* Hint */
+                int hint_len;
+                printf("#    Hint: ");
                 break;
             case 4:
-                /* Head */
+                /* Head + Hint code*/
                 break;
             case 5:
                 /* Upper */
+                if (*lives <= 3)
+                {
+                    printf("#                               /|\\  |  #");
+                }
+                else if (*lives == 4)
+                {
+                    printf("#                               /|   |  #");
+                }
+                else if (*lives == 5)
+                {
+                    printf("#                                |   |  #");
+                }
+                else
+                {
+                    printf("#                                    |  #");
+                }
                 break;
             case 6:
-                /* Middle */
+                /* Middle  + Hidden Word */
+                if (*lives > 2)
+                {
+                    /* no middle */
+                }
+                else
+                {
+                    /* have middle */
+                }
                 break;
             case 7:
-                /* Legs */
+                /* Legs  + Number Code*/
                 if (*lives == 0)
                 {
                     printf("#                               / \\  |  #");
