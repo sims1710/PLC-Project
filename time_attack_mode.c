21219dc945f5a33492b1781d1a3922b5f66f3969
ShelenGo <shelengo21@gmail.com>
2024-04-03 14:14:06 +0800

    update time attack mode

--- time_attack_mode.c
+++ time_attack_mode.c
@@ -4,54 +4,41 @@
 #include <string.h>
 #include "all_functions.h"
 
-void time_attack_mode(char *chosen_word, int time_limit_seconds){
+void time_attack_mode(char *chosen_word, char *hidden_word, char *guessed_letters, int *lives, int word_len){
     int i;
-    time_t start_time, current_time;
-    int lives = 7;
-    int word_len = strlen(chosen_word);
-    char *hidden_word = malloc(word_len + 1);
-    char *guessed_letters = malloc(26); /*Allocate space for each letter of the alphabet*/
+    time_t start_time, current_time, time_left;
+    /*4 minutes (360 seconds) for the player to guess the word in this time attack mode*/
+    time_t fixed_time_limits = 360;
 
-    /*Check if memory allocation was successful*/
-    if (hidden_word == NULL || guessed_letters == NULL) {
-        fprintf(stderr, "Failed to allocate memory.\n");
-        free(hidden_word); 
-        free(guessed_letters); 
-        return;
-    }
-
-    /*Initialize hidden_word with underscores or a similar placeholder*/
-    for (i = 0; i < word_len; ++i) {
-        hidden_word[i] = '_';
-    }
-    hidden_word[word_len] = '\0';
-
-    /*Initialize guessed_letters to 0 to track which letters have been guessed*/
-    memset(guessed_letters, 0, 26 * sizeof(char));
-
-    time(&start_time); /*Record start time*/
+    /*Record start time*/
+    time(&start_time); 
 
     while (lives > 0) {
         printf("Guess the word: %s\n", hidden_word);
         player_input(chosen_word, hidden_word, guessed_letters, &lives, word_len);
 
         time(&current_time); /*Check current time*/
-        if (difftime(current_time, start_time) > time_limit_seconds) {
+        /*Printing out the remaining time left for the player*/
+        if(difftime(current_time, start_time) < fixed_time_limits) {
+            time_left = fixed_time_limits - current_time;
+            printf("You still have %s time left!", ctime(&time_left));
+        }
+
+        /*Checking if the player time exceed the game time, if it exceeds, Time's up*/
+        if (difftime(current_time, start_time) > fixed_time_limits) {
             printf("Time's up! You didn't guess the word in time.\n");
             break;
         }
 
+        /*Checking if the word guessed and the chosen word match to know if the player wins or not*/
         if (strcmp(chosen_word, hidden_word) == 0) {
             printf("Congratulations! You've guessed the word: %s\n", chosen_word);
             break;
         }
     }
 
+    /*Checking if player has enough lives to play with*/
     if (lives <= 0) {
         printf("Out of lives! The word was: %s\n", chosen_word);
     }
-
-    /*Free the dynamically allocated memory*/
-    free(hidden_word);
-    free(guessed_letters);
 }
