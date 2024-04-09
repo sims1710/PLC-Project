016cc9f2792b8b0bc81b062f0646f8f87e0af2a4
Simriti <simritib1@gmail.com>
2024-04-09 13:37:40 +0800

    Update ui.h

--- ui.h
+++ ui.h
@@ -1,8 +1,13 @@
 /* Code to open the HTML File */
 #ifdef _WIN32
+// Includes necessary headers for Windows-specific functionality
 #include <windows.h>
 #elif __APPLE__
+// Includes necessary headers for Apple (macOS)-specific functionality
 #include <stdlib.h>
+#elif __linux__
+// Includes necessary headers for Linux-specific functionality
+#include <unistd.h>
 #endif
 
 void displayRules();
